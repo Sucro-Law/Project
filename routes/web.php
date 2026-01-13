76cf589264e76972deeca34e6cd7c1954ebc210d
JKVelayooo <josefvelayo@gmail.com>
2026-01-13 18:00:48 +0800

    Add PUP student sign-in and sign-up page UI

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/signin', function () {
+    return view('authfolder.signin');
+});
+
+Route::get('/signup', function () {
+    return view('authfolder.signup');
+});
