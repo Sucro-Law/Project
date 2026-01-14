c1ece467dbf1911cd9e2367a629bfd99f5c07a02
JKVelayooo <josefvelayo@gmail.com>
2026-01-14 20:57:15 +0800

    Routes for blade

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrgController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,10 +9,36 @@ Route::get('/', function () {
 
 
 
-Route::get('/signin', function () {
+Route::get('/login', function () {
     return view('authfolder.signin');
 });
 
 Route::get('/signup', function () {
     return view('authfolder.signup');
+})->name('signup');
+
+Route::get('/dashboard', [OrgController::class, 'index'])->name('dashboard');
+
+
+Route::get('/membership', function () {
+    return view('afterloginfolder.memberform');
+});
+
+/*Route::get('/dashboard', function () {
+    return view('afterloginfolder.dashboard');
+});*/
+
+
+Route::get('/orgDetail', function () {
+    return view('afterloginfolder.orgDetail');
+})->name('orgDetail');
+
+
+#----REFERENCES URL--
+Route::get('/dashboard2', function () {
+    return view('pureHTML.dash1');
+});
+
+Route::get('/orgDetailRef', function () {
+    return view('pureHTML.refOrgDetail');
 });
