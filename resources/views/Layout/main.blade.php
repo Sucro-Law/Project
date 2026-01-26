9fb6799852e9d7638e5e8198a6ae6dda524fcb36
9 <kenjohnvianneym@gmail.com>
2026-01-26 15:08:23 +0800

    Update main.blade.php

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -27,7 +27,6 @@
         <div class="main-content">
             @yield('content')
             @include('layout.sidebar')
-            @include('layout.footer')
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
