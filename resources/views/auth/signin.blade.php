65068cb3a068fee05b9f931b3c1ba63dcd08e0fc
Evans <johnevansgutierrez9@gmail.com>
2026-01-27 14:06:58 +0800

    vite signin

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/signin.css', 'resources/js/app.js'])
     
 </head>
 
