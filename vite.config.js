18400aa725a1ae874d6116057280ff331ec1a5fc
JKVelayooo <josefvelayo@gmail.com>
2026-01-27 13:09:01 +0800

    vite
    vite

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,23 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+
+                'resources\css\admin.css',
                 'resources/css/app.css',
                 'resources/css/dashboard.css',
-                /*'resources/css/layout.css',*/
+                'resources/css/event.css',
+                'resources/css/forgotpass.css',
                 'resources/css/orgdesc.css',
+                'resources/css/profile.css',
+                'resources/css/settings.css',
+                'resources/css/signin.css',
+                'resources/css/signup.css',
+
+                /*'resources/css/layout.css',*/
                 /*'resources/css/pages.css',*/
+
                 'resources/js/app.js',
+                
             ],
             refresh: true,
         }),
